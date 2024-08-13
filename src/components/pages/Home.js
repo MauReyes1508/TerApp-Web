7bb1fe35b0e1e365ce25178702ee7ce378e6950f
Jose <arrietajoseangel50@gmail.com>
2024-08-13 13:23:48 -0500

    actualizacion

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,44 @@
-// Home.js
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import '../CSS/Home.css';
+import logo from '../img/logo.jpeg';
+import icono from '../img/icono.png';
 
 const Home = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    // Lógica para obtener datos
-    fetch('/api/data')
-      .then(response => response.json())
-      .then(data => setData(data));
-  }, []);
-
   return (
     <div>
-      <h1>Home Page</h1>
-      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
+      <div className="header">
+        <div className="container">
+          <div className="logo-section">
+            <img src={icono} alt="Logo" className="logo" />
+            <div className="title">TerApp</div>
+          </div>
+          <div className="buttons">
+            <a href="/iniciosesion2/iniciosesion2.html">
+              <button className="btn">Iniciar sesión</button>
+            </a>
+            <a href="/registro/registrosesion.html">
+              <button className="btn">Registrarse</button>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="main-container">
+        <div className="text-center">
+          <img src={logo} alt="Logo de la aplicación" className="main-logo" />
+          <h2 className="subtitle">TerApp Movil</h2>
+          <p className="description">
+            TerApp es una aplicación de acompañamiento para la rehabilitación, enfocada en personas que hayan sufrido fracturas. 
+            Contiene inteligencia artificial que colaborará para medir el avance de la recuperación. Incluye módulos específicos 
+            para cada tipo de rehabilitación. <br /><br /> ¡Descarga Aquí!
+          </p>
+        </div>
+        <div className="text-center">
+          <a href="/inicio/descarga.html">
+            <button className="btn download-btn">Descargar</button>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
